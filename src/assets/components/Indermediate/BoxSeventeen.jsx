1eda4ae0923d07e22282f9092cb5f03d1c7a3c4e
Ritesh Mishra <riteshmishra9565@gmail.com>
2026-01-21 12:21:56 +0530

    Refactor BoxSeventeen loader animation for improved exit transitions and styling

--- src/assets/components/Indermediate/BoxSeventeen.jsx
+++ src/assets/components/Indermediate/BoxSeventeen.jsx
@@ -22,18 +22,32 @@ const BoxSeventeen = () => {
         onAnimationComplete={() => setLoader(true)}
       >
         {loader && (
-          <motion.div
-            //   initial={{ scale: 0, opacity: 0, rotate: 0 }}
-            animate={{ scale: 1, opacity: 1, rotate: [0, 360] }}
-            transition={{
-              duration: 0.8,
-              ease: "linear",
-              repeat: Infinity,
-            }}
-            className="h-20 w-20 rounded-full border-l-2 border-t-2 pointer-events-none"
-          >
-            {/* <div className="w-20 h-20 border-t-2 border-b-2 border-l-2 bg-transparent rounded-full"></div> */}
-          </motion.div>
+          <AnimatePresence>
+            <motion.div
+              initial={{ y: 50, opacity: 0, scale: 0.7 }}
+              animate={{ y: 0, scale: 1, opacity: 1 }}
+              transition={{
+                duration: 0.5,
+                type: "spring",
+                damping: 5,
+                stiffness: 100,
+              }}
+              exit={{ y: 50, opacity: 0, scale: 0.7 }}
+            >
+              <motion.div
+                //   initial={{ scale: 0, opacity: 0, rotate: 0 }}
+                animate={{ scale: 1, opacity: 1, rotate: [0, 360] }}
+                transition={{
+                  duration: 0.8,
+                  ease: "linear",
+                  repeat: Infinity,
+                }}
+                className="h-20 w-20 rounded-full border-l-4 border-t-4 pointer-events-none"
+              >
+                {/* <div className="w-20 h-20 border-t-2 border-b-2 border-l-2 bg-transparent rounded-full"></div> */}
+              </motion.div>
+            </motion.div>
+          </AnimatePresence>
         )}
       </motion.div>
     </div>
